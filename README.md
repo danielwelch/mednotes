6a29d21cea1e911dbbc64335490299d9da813a86
Daniel Welch <dwelch2102@gmail.com>
2016-02-08 11:49:56 -0500

    added port default set

--- README.md
+++ README.md
@@ -6,5 +6,5 @@ I use this to quickly view all of my Quiver notes from other devices.
 
 #### Workflow
 1. Take notes with [Quiver](http://happenapps.com/#quiver)
-2. Export a notebook as HTML to the MedNotes base directory
+2. Export a notebook as HTML to the MedNotes notebooks directory
 3. Push the changes to the server
--- main.go
+++ main.go
@@ -9,9 +9,15 @@ import (
 
 func main() {
 
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+		log.Println("[-] No PORT environment variable detected. Setting to ", port)
+	}
+
 	r := handlers.LoggingHandler(os.Stdout, Router())
 
 	log.Println("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":"+os.Getenv("PORT"), r))
+	log.Fatal(http.ListenAndServe(":"+port, r))
 
 }
